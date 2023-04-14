a05a0485556a804ade4e156fd08add5b831bba9e
brwilkinson <brwilkinson@outlook.com>
2023-04-14 13:20:54 -0700

    add note about UAI requirement

--- setup_deploy.ps1
+++ setup_deploy.ps1
@@ -1,7 +1,12 @@
 <# 
 setx.exe 'BICEP_TRACE' '1'
-setx.exe 'BICEP_TRACE_LEVEL' 'Verbose'
+setx.exe 'BICEP_TRACING_VERBOSITY' 'Full'
 Write-Output 'requires restart'
+
+I opted not to create the User assigned identity in this project
+
+bicep/main.bicep#L33 <-- create this User assigned identity and provide it read access to your resource e.g. RG
+
 #>
 
 $Base = $PSScriptRoot
